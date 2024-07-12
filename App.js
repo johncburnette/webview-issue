5b256d43f0eb6802b2d43a013fe062d93435d0ed
John Burnette <john.burnette@revcontent.com>
2024-07-12 10:39:35 -0400

    feat(*): partial changes to consume various scroll/positional data

--- App.js
+++ App.js
@@ -13,11 +13,11 @@ import RevcontentWidget, {
 import RevcontentNativeView from './components/RevcontentNativeView';
 
 export default function App() {
-const widgetRef = useRef(null);
+  const widgetRef = useRef(null);
 
-const onScroll = event => {
-  widgetRef.current?.onScroll(event.nativeEvent);
-};
+  const onScroll = event => {
+    widgetRef.current?.onScroll(event.nativeEvent);
+  };
   const onExperimentalScroll = useCallback(
     () => onRevcontentVisibilityChange(),
     []
@@ -86,12 +86,12 @@ const onScroll = event => {
           enim diam. Mauris vitae ultricies leo integer malesuada. Suspendisse
           sed nisi lacus sed viverra tellus in.
         </Text>
-<RevcontentNativeView
-  ref={widgetRef}
-  widgetId={66621}
-  pubId={84088}
-  siteUrl={'https://app.revcontent.com'}
-/>
+        <RevcontentNativeView
+          ref={widgetRef}
+          widgetId={66621}
+          pubId={84088}
+          siteUrl={'https://app.revcontent.com'}
+        />
 
         {/* <RevcontentWidget
           widgetId={66621}
--- components/RevcontentNativeView/index.js
+++ components/RevcontentNativeView/index.js
@@ -1,7 +1,14 @@
 import WebView from 'react-native-webview';
 import { useAssets } from 'expo-asset';
 import { readAsStringAsync } from 'expo-file-system';
-import { useRef, useState, forwardRef, useImperativeHandle } from 'react';
+import {
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useEffect
+} from 'react';
+import { Text, View } from 'react-native';
 
 const getInjectedMessage = message => {
   return `
@@ -17,11 +24,34 @@ const RevcontentNativeView = forwardRef(({ widgetId, pubId, siteUrl }, ref) => {
   if (!widgetId) throw new TypeError('You must supply a valid widget ID');
   if (!pubId) throw new TypeError('You must supply a valid publisher ID');
   if (!siteUrl) throw new TypeError('You must supply a valid site URL');
-
+  if (!ref)
+    throw new TypeError(
+      'RevcontentNativeView requires a ref to dispatch scroll position'
+    );
+  const [index] = useAssets(require('./assets/widget.html'));
+  const viewRef = useRef(null);
+  const webViewRef = useRef(null);
   const [html, setHtml] = useState('');
   const [viewHeight, setViewHeight] = useState(0);
-  const webViewRef = useRef(null);
-  const [index] = useAssets(require('./assets/widget.html'));
+  const [screenDimensions, setScreenDimensions] = useState({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0
+  });
+  const [scrollDimensions, setScrollDimensions] = useState({
+    contentInset: { bottom: 0, left: 0, right: 0, top: 0 },
+    contentOffset: { x: 0, y: 0 },
+    contentSize: { height: 0, width: 0 },
+    layoutMeasurement: { height: 0, width: 0 },
+    zoomScale: 1
+  });
+  const [relativeLayout, setRelativeLayout] = useState({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0
+  });
 
   if (index)
     readAsStringAsync(index[0].localUri).then(data => {
@@ -36,18 +66,87 @@ const RevcontentNativeView = forwardRef(({ widgetId, pubId, siteUrl }, ref) => {
   };
 
   useImperativeHandle(ref, () => ({
-    onScroll: data =>
-      webViewRef.current.injectJavaScript(getInjectedMessage(data))
+    onScroll: data => {
+      //save scroll values to state
+      setScrollDimensions(data);
+
+      //measure the relative position to state
+      viewRef.current.measure((x, y, width, height) =>
+        setRelativeLayout({
+          x,
+          y,
+          width,
+          height
+        })
+      );
+      // We need to calculate the content position with the offest to determine where the
+      // top position of the widget is so we can try to then figure out what should be considered
+      // in view
+      const contentOffsetWithLayout =
+        data.contentOffset.y + data.layoutMeasurement.height;
+      const isWidgetIntersecting = contentOffsetWithLayout >= relativeLayout.y;
+
+      webViewRef.current.injectJavaScript(
+        getInjectedMessage({
+          action: 'scroll',
+          payload: data
+        })
+      );
+    }
   }));
 
+  useEffect(() => {
+    viewRef.current?.measureInWindow((x, y, width, height) => {
+      // limit state update to only when any dimension doesn't match what's current in state
+      if (
+        x !== screenDimensions.x ||
+        y !== screenDimensions.y ||
+        width !== screenDimensions.width ||
+        height !== screenDimensions.height
+      ) {
+        setScreenDimensions({ x, y, width, height });
+      }
+    });
+  });
+
+  useEffect(() => {
+    //send back screen dimensions
+    webViewRef.current.injectJavaScript(
+      getInjectedMessage({
+        action: 'screen',
+        payload: screenDimensions
+      })
+    );
+  }, [screenDimensions]);
+
   return (
-    <WebView
-      ref={webViewRef}
-      originWhitelist={['*']}
-      scrollEnabled={false}
-      onMessage={onMessage}
-      style={{ flex: 1, height: viewHeight }}
-      source={{ html }}></WebView>
+    <View
+      ref={viewRef}
+      style={{
+        flex: 1,
+        height: viewHeight
+      }}>
+      <View
+        style={{
+          position: 'relative',
+          top: 0,
+          right: 0,
+          left: 0,
+          zIndex: 123234,
+          backgroundColor: '#e7e7e7',
+          padding: 15
+        }}>
+        <Text>{JSON.stringify(screenDimensions)}</Text>
+        <Text>{JSON.stringify(scrollDimensions)}</Text>
+      </View>
+      <WebView
+        ref={webViewRef}
+        originWhitelist={['*']}
+        scrollEnabled={false}
+        onMessage={onMessage}
+        style={{ flex: 1, height: viewHeight }}
+        source={{ html }}></WebView>
+    </View>
   );
 });
 
